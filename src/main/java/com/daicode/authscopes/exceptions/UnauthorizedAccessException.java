9850c321919224cc390879608fee8a5c8fffe7f7
eimbachi <eimbachi@emeal.nttdata.com>
2025-03-26 18:19:17 -0500

    Update UnauthorizedAccessException.java
    delete ResponseStatus in UnauthorizedAccessException

--- src/main/java/com/daicode/authscopes/exceptions/UnauthorizedAccessException.java
+++ src/main/java/com/daicode/authscopes/exceptions/UnauthorizedAccessException.java
@@ -2,10 +2,7 @@ package com.daicode.authscopes.exceptions;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.FORBIDDEN)
 @Getter
 @Setter
 public class UnauthorizedAccessException extends RuntimeException {
